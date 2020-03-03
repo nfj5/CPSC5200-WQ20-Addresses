cfdcef18d288361cc31ecd752bdded097ab4c5ca
Nick Jones <28287121+nfj5@users.noreply.github.com>
2020-03-02 19:33:06 -0800

    Add check to see if country exists in config file

--- parser/parser.py
+++ parser/parser.py
@@ -5,13 +5,21 @@ import re
 
 def get_format(country_iso):
     country_iso = country_iso.upper()
+
+    # make sure that the country matches format guidelines
     match = re.match(r'[A-Z]{2}', country_iso, flags=0)
     if not match:
         print("ERROR: Invalid ISO country!")
         exit(1)
+
     with open('address.json', encoding='utf-8') as file:
         data = json.load(file)
         countries = data['options']
+
+        # check if the country exists in the configuration file
+        if country_iso not in [country['iso'] for country in countries]:
+            print("Country is not present in configuration file!")
+            exit(1)
 
         for country in countries:
             if country['iso'] == country_iso:
@@ -23,5 +31,4 @@ if __name__ == '__main__':
         print("Need a country ISO as argument")
         exit(1)
 
-    format = get_format(sys.argv[1])
-    print(format)
+    print(get_format(sys.argv[1]))
