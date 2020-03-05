ab890364af6efad1c7a2fbc59b009ae80cb14e8b
Nick Jones <28287121+nfj5@users.noreply.github.com>
2020-03-05 12:59:30 -0800

    Simplify format load flow, add response codes

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 import json
 import re
 import pymongo
@@ -9,10 +9,32 @@ mongo = pymongo.MongoClient("mongodb://localhost:27017/")
 db = mongo["CPSC5200-WQ"]
 address_collection = db["Addresses"]
 
+addr_formats = None
 
-@app.route('/')
-def hello_world():
-	return 'Hello World!'
+
+# construct a Flask Response object
+def get_response(code, content):
+	return Response(json.dumps(content), status=code, mimetype="application/json")
+
+
+# load address formats from a file
+def load_formats():
+	global addr_formats
+
+	file = open('addresses2.json', encoding='utf-8')
+	addr_formats = json.load(file)
+	file.close()
+
+
+# get the address format for a specific ISO code
+def get_format(country):
+	if addr_formats is None:
+		load_formats()
+
+	if country not in addr_formats:
+		return None
+
+	return addr_formats[country]
 
 
 # verify that the provided address matches the format of the country provided
@@ -23,53 +45,45 @@ def verify_address(address, country_code):
 		print("ERROR: Invalid ISO country!")
 		return False
 
-	with open('addresses2.json', encoding='utf-8') as file:
-		data = json.load(file)
+	addr_format = get_format(country_code)
 
-		# make sure that the country is present
-		if country_code not in data.keys():
-			print("Country is not present in configuration file!")
+	# make sure that the format is present
+	if addr_format is None:
+		print("Country is not present in configuration file!")
+		return False
+
+	# make sure that this field isn't something that isn't in the format
+	for field in address:
+		if field not in addr_format and field != "Country":
+			print("extra field present", field)
 			return False
 
-		# try to find the country
-		for country in data.keys():
-			if country == country_code:
-				addr_format = data[country]
+	for field in addr_format.keys():
+		# make sure that we are not missing required fields
+		if field not in address and addr_format[field] != "":
+			print("missing field", field)
+			return False
 
-				# make sure that this field isn't something that isn't in the format
-				for field in address:
-					if field not in addr_format and field != "Country":
-						print("extra field present", field)
-						return False
+		# if the format has required values, check against those
+		if isinstance(addr_format[field], dict):
+			if not address[field] in addr_format[field].keys():
+				print("not matching options", address[field])
+				return False
 
-				for field in addr_format.keys():
-					# make sure that we are not missing required fields
-					if field not in address and addr_format[field] != "":
-						print("missing field", field)
-						return False
+		# otherwise, check against the provided regex if field not required
+		elif addr_format[field] != "":
+			regex = addr_format[field]
+			if not re.match(regex, str(address[field]), flags=0):
+				print("not matching regex", address[field])
+				return False
 
-					# if the format has required values, check against those
-					if isinstance(addr_format[field], dict):
-						if not address[field] in addr_format[field].keys():
-							print("not matching options", address[field])
-							return False
-
-					# otherwise, check against the provided regex if field not required
-					elif addr_format[field] != "":
-						regex = addr_format[field]
-						if not re.match(regex, str(address[field]), flags=0):
-							print("not matching regex", address[field])
-							return False
-
-				return True
-
-	return False
+	return True
 
 
 @app.route('/addresses', methods=['POST'])
 def insert_address():
 	if not request.json:
-		return {"message": "Failed to insert address", "result": "No request body."}
+		return get_response(400, {"message": "Failed to insert address", "result": "No request body."})
 	data = request.json
 
 	num_inserted = 0
@@ -87,7 +101,7 @@ def insert_address():
 		else:
 			num_failed += 1
 
-	return {"message": "Success", "result": "Inserted "+str(num_inserted)+" addresses. "+str(num_failed)+" not inserted."}
+	return get_response(200, {"message": "Success", "result": "Inserted "+str(num_inserted)+" addresses. "+str(num_failed)+" not inserted."})
 
 
 @app.route('/addresses')
@@ -96,7 +110,21 @@ def get_addresses():
 	for item in address_collection.find({}, {"_id": 0}):
 		t_list.append(item)
 
-	return {"message": "Success", "result": t_list}
+	return get_response(200, {"message": "Success", "result": t_list})
+
+
+@app.route('/addresses/<string:country>')
+def get_by_country(country):
+	addr_format = get_format(country)
+
+	if addr_format is None:
+		return get_response(400, {"message": "Failure", "result": "Country is not currently handled by this API."})
+
+	t_list = []
+	for item in address_collection.find({"Country": country}, {"_id": 0}):
+		t_list.append(item)
+
+	return get_response(200, {"message": "Success", "result": t_list})
 
 
 if __name__ == '__main__':
