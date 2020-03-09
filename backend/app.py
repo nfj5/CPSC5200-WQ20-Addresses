3130a41015bac2d344217f632a92242bf911a866
Nick Jones <28287121+nfj5@users.noreply.github.com>
2020-03-09 16:07:35 -0700

    Further Swagger documentation

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, Response
 from flask_cors import CORS
-from flask_restplus import Api, Resource
+from flask_restplus import Api, Resource, fields
 from bson.errors import InvalidId
 from bson.objectid import ObjectId
 import json
@@ -17,7 +17,7 @@ app = Api(
 		title="AddressAPI",
 		description="API for storing/updating/retrieving world addresses.")
 
-namespace = app.namespace('addresses', description='Main API version ' + VERSION)
+namespace = app.namespace('addresses', description='Main API interface')
 format_namespace = app.namespace('formats', description='Address format endpoints')
 
 mongo = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -26,7 +26,8 @@ address_collection = db["Addresses"]
 
 addr_formats = None
 
-# region Helper Functions
+# region Helper Code
+
 
 # construct a Flask Response object
 def get_response(code, content):
@@ -108,6 +109,34 @@ def verify_address(address, country_code):
 
 	return True
 
+
+example_address = app.model('AddressPostModel', {
+	'Country': fields.String(required=True, example='CA'),
+	'Address1': fields.String(example='14 Albertson Lane'),
+	'Address2': fields.String(example='Apartment 240'),
+	'City': fields.String(example='Vancouver'),
+	'Province': fields.String(example='BC'),
+	'PostalCode': fields.Integer(example=35421)
+})
+
+example_address_put = app.model('AddressPutModel', {
+	'Address2': fields.String(example='Apartment 243')
+})
+
+example_response = app.model('AddressPostResponseModel', {
+	'Country': fields.String(required=True, example='CA'),
+	'Address1': fields.String(example='14 Albertson Lane'),
+	'Address2': fields.String(example='Apartment 240'),
+	'City': fields.String(example='Vancouver'),
+	'Province': fields.String(example='BC'),
+	'PostalCode': fields.Integer(example=35421),
+	'_id': fields.String(example='5e6096e3b0cbe6aa74b93c19')
+})
+
+example_response_put = app.inherit('AddressPutResponseModel', example_response, {
+	'Address2': fields.String(example='Apartment 243')
+})
+
 # endregion
 
 
@@ -123,6 +152,7 @@ class GetFormats(Resource):
 @namespace.route('/')
 class Addresses(Resource):
 	# allow the user to retrieve all stored addresses
+	@app.doc(responses={200: "Success", 400: "Invalid request format"})
 	def get(self):
 		query = {}
 		for arg in request.args:
@@ -136,6 +166,10 @@ class Addresses(Resource):
 		return get_response(200, t_list)
 
 	# allow the user to insert an address following country formats
+	# @app.doc(responses={200: "Success", 400: "Invalid request format"}, example=example_response)
+	@app.response(200, 'Success', example_response)
+	@app.response(400, 'Invalid request format')
+	@app.expect(example_address)
 	def post(self):
 		if not request.json:
 			return get_response(400, {"error": "No request body."})
@@ -158,6 +192,8 @@ class Addresses(Resource):
 @namespace.route('/<string:address_id>')
 class AddressById(Resource):
 	# get a specific address based upon the identifier
+	@app.param('address_id', 'The specified address identifier')
+	@app.doc(responses={200: "Success", 400: "Invalid request format"})
 	def get(self, address_id):
 		try:
 			result = address_collection.find({"_id": ObjectId(address_id)})
@@ -169,6 +205,10 @@ class AddressById(Resource):
 			return get_response(400, {"error": "Invalid ObjectId sequence"})
 
 	# allow the user to update the specified address
+	@app.param('address_id', 'The specified address identifier')
+	@app.response(200, 'Success', example_response_put)
+	@app.response(400, 'Invalid request format')
+	@app.expect(example_address_put)
 	def put(self, address_id):
 		if not request.json:
 			return get_response(400, {"error": "No request body."})
@@ -201,6 +241,8 @@ class AddressById(Resource):
 # allow the user to search based on a country specific format
 @namespace.route('/country/<string:country>')
 class GetByCountry(Resource):
+	@app.param('country', 'The country to search in')
+	@app.doc(responses={200: "Success", 400: "Invalid request format"})
 	def get(self, country):
 		addr_format = get_format(country)
 
